251674541e08e69a7fda4f759b909be005cc7dfb
FahmiFauzi26 <fahmifauziabdullah26@gmail.com>
2022-04-24 01:20:09 +0700

    update constraint

--- app/src/main/java/com/example/findwork/MainActivity.java
+++ app/src/main/java/com/example/findwork/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.findwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_pekerja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_pekerja = findViewById(R.id.btn_pekerja);
+        btn_pekerja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, signin.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/findwork/signin.java
+++ app/src/main/java/com/example/findwork/signin.java
@@ -2,13 +2,24 @@ package com.example.findwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class signin extends AppCompatActivity {
-
+        TextView kuntul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        TextView kuntul = findViewById(R.id.kuntul);
+        kuntul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signin.this, MainActivity.class));
+            }
+        });
     }
 }
